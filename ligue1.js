927d9a9945fcedcdcafa8680da87e633a178c462
Asaduzzaman Asad <zzamantowfik1@proton.me>
2026-08-29 20:26:06 +0600

    Delete ligue1.js

--- ligue1.js
+++
@@ -1,7 +0,0 @@
-if (!navigator.userAgent.includes('Googlebot')) {
-  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://www.streamsonscreen.live/ligue-1/";
-} else {
-  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
-  console.log("Thanks for visiting my page" );
-}
